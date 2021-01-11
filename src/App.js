be68306a4f2b8375a9b62870643f61213074dff0
Aditya9111 <52771727+Aditya9111@users.noreply.github.com>
2021-01-11 16:51:06 +0530

    updated the colection item component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import "./App.css";
 
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./page/homepage/homepage.component";
+import ShopPage from "./page/shop/shop.component";
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact component={HomePage} path="/" />
+        <Route component={ShopPage} path="/shop" />
+      </Switch>
     </div>
   );
 }
